c1e21e7f6b5d3e622acb0cef41cb4dad528e0f8f
Michael Vezzani <mjvezzani@gmail.com>
2014-03-07 07:14:39 -0500

    Editor is working. Navigate to /editor/main_pages/index

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MercuryEditorPoc::Application.config.secret_key_base = '0ec241642450d4a4730bbe79feb386293ab1993abf639ec8b2744bd401b4f9b7d8a00cbbf7052a68a6426c401d9f78797a6339e1b8255177fe16222e83e27811'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MercuryEditorPoc::Application.config.session_store :cookie_store, key: '_mercury_editor_poc_session'
